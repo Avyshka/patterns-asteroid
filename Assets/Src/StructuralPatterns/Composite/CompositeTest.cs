d6cd8fa46e79e361b56e0491164960db8a51795f
Aivar <avyshka@yandex.ru>
2021-10-11 23:10:25 +0300

    Added structural patterns

---
+++ Assets/Src/StructuralPatterns/Composite/CompositeTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+namespace Composite
+{
+    public sealed class CompositeTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            IAttack attack = new Unit();
+            Detachment attacks = new Detachment();
+            attacks.AddUnit(attack);
+            
+            
+            attack.Attack();
+            attacks.Attack();
+        }
+    }
+}
